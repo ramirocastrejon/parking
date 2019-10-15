5daf6e513b4a6db1512b5a392ee2bc009fdcbc45
ramirocastrejon <47700941+ramirocastrejon@users.noreply.github.com>
2019-10-15 13:16:43 -0500

    added Pay, Ticket, ParkingLot, and Vehicle classes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 	// write your code here
         System.out.println("This will be a parking lot model");
+        Pay newPay = new Pay();
+        newPay.setEntry();
+        TimeUnit.SECONDS.sleep(5);
+        newPay.setExit();;
+        newPay.calculateAmountDue();;
+        System.out.println("Amount due is : " + newPay.payAmountDue());
     }
 }
